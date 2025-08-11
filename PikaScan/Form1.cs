dfd1b487a281ccb2cdc7a1c57d99b060e0ffc98d
OswaldoDG <odiaz@arguz.com>
2025-08-11 07:20:55 -0600

    Inicial

---
+++ PikaScan/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PikaScan
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string deeplink)
+        {
+            InitializeComponent();
+            MessageBox.Show("Deeplink recibido: " + deeplink);
+        }
+    }
+}
---
+++ PikaScan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PikaScan
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string deeplink = args.Length > 0 ? args[0] : null;
+
+            Application.Run(new Form1(deeplink));
+        }
+    }
+}
