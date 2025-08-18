ffa76aa36eff4186a36ec060123ca9174e9a6d22
OswaldoDG <odiaz@arguz.com>
2025-08-18 08:46:08 -0600

    Servicio Upload PIKA

---
+++ PikaScan/Servicios/pikaapi/PaginaPika.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PikaScan.Servicios.pikaapi
+{
+    public class PaginaPika
+    {
+        public string Ruta { get; set; }
+    }
+}
---
+++ PikaScan/Servicios/pikaapi/UploadApi.cs
@@ -0,0 +1,24 @@
+﻿using PikaScan.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PikaScan.Servicios.pikaapi
+{
+    public class UploadApi
+    {
+
+        public async Task EnviarPaginas(List<PaginaPika> paginas, string token)
+        {
+            // Generar un nuvo GUID string a utilizar como TransactionId
+            // llamar a UploadController 
+            // [HttpPost("scanner/pagina")] 
+            // Con cada una de las páginas convetidas a  ElementoCargaContenido
+            // Y al finalizzar llamra [HttpPost("scanner/completar/{TransaccionId}")]
+
+        }
+
+    }
+}
